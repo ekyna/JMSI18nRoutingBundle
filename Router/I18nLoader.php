731c1aaa762510d65f0641f356258a8c8d46944e
Etienne Dauvergne <contact@ekyna.com>
2015-05-28 16:19:33 +0200

    FOSJsRoutingBundle BC

---
+++ Router/I18nLoader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace JMS\I18nRoutingBundle\Router;
+
+use JMS\I18nRoutingBundle\Router\Loader\I18nLoader as BaseLoader;
+
+/**
+ * To not break FOSJsRoutingBundle compatibility.
+ *
+ * @package JMS\I18nRoutingBundle\Router
+ * @author Étienne Dauvergne <contact@ekyna.com>
+ */
+class I18nLoader extends BaseLoader
+{
+}
